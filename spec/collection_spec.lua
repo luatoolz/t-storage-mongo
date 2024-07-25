c36ad3596229fd13b5fc55690031c821ea50b8d5
luatoolz <luatoolz@somahorse.com>
2024-07-25 15:59:42 +0300

    debug spec

--- spec/collection_spec.lua
+++ spec/collection_spec.lua
@@ -98,6 +98,8 @@ describe("collection", function()
     assert.equal(coll[''], coll[{}])
     assert.equal(coll['*'], coll[{}])
 
+    assert.equal('', {table.map(coll['*']), table.map(coll['']), table.map(coll[{}]) })
+
     assert.same(table.map(coll['']), table.map(coll[{}]))
     assert.same(table.map(coll['*']), table.map(coll[{}]))
   end)
