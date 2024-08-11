f3550226f10418b9f12b9fea018241b7db079928
luatoolz <luatoolz@somahorse.com>
2024-08-11 07:05:54 +0300

    fix: simpler init

--- lua/t/storage/mongo/init.lua
+++ lua/t/storage/mongo/init.lua
@@ -1,17 +1,18 @@
 local pkg = (...) or 't.storage.mongo'
+local meta = require "meta"
 local t = require "t"
-local is = t.is ^ 'mongo'
 local driver = require "mongo"
 assert(driver, 'failed to load mongo driver')
+local _ = meta.loader(pkg).type
 
 -- __  = t.storage.mongo.connection
 -- ___ = db name
 return t.object({
   __name='t/storage/mongo',
-  __toboolean=function(self) return is.factory(self) and toboolean(self()) or toboolean(driver.Client(tostring(self.__)):getDatabaseNames()) end,
+  __toboolean=function(self) return type(next(self))=='nil' and toboolean(self()) or toboolean(driver.Client(tostring(self.__)):getDatabaseNames()) end,
   __div = function(self, k)
     if type(k)~='string' or k=='' then return nil end
-    if is.factory(self) then return self()/k end
+    if type(next(self))=='nil' then return self()/k end
     self.___ = k
     return self
   end,
@@ -21,7 +22,7 @@ return t.object({
   end,
 }):postindex(function(self, k)
   if type(k)~='string' or k=='' or k=='__' or k=='___' then return nil end
-  if is.factory(self) then return driver[k] or self()[k] end
+  if type(next(self))=='nil' then return driver[k] or self()[k] end
   assert(self.collection, 'require self.collection')
   return self.collection({conn=self.__, db=self.___, k=k})
 end)
