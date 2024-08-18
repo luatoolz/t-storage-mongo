2ec185aee5e0f2c75f3781bb75adef22e6699a27
luatoolz <luatoolz@somahorse.com>
2024-08-19 01:21:12 +0300

    refactor records.lua

--- lua/t/storage/mongo/records.lua
+++ lua/t/storage/mongo/records.lua
@@ -1,9 +1,10 @@
-local t = require "t"
+local t = t or require "t"
 local is = t.is
 local iter = require "t.storage.mongo.iter"
+local json = t.format.json
+local jsoner = function(x) return tojson(x, true) end
 
 return setmetatable({}, {
---  __add=function(self, o) if type(o)=='number' then return tonumber(self)+o end end,
   __call=function(self, coll, query)
     assert(is.storage.mongo.collection(coll))
     assert(query)
@@ -16,7 +17,8 @@ return setmetatable({}, {
   __len=function(self) return tonumber(self) end,
   __mod=table.filter,
   __mul=table.map,
-  __pairs=function(self) local i, it = 1, iter(self)
+  __name='t/storage/mongo/records',
+  __pairs=function(self) local i, it = 0, iter(self, jsoner)
     return function() local rv=it(); i=rv and i+1 or nil; return i,rv end end,
   __sub=function(self, o)
     if rawequal(self, o) or o=='*' or is.table.empty(o) then o=self.query end
@@ -25,5 +27,7 @@ return setmetatable({}, {
   end,
   __toboolean=function(self) return tonumber(self)>0 end,
   __tonumber=function(self) return self.__:count(self.query) or 0 end,
+  __tojson=function(self) return table.map(iter(self, jsoner)) end,
+  __toJSON=function(self) return table.map(iter(self, jsoner)) end,
   __unm=function(self) return self.__:remove(self.query) end,
 })
