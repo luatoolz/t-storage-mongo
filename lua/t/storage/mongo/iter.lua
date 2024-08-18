09780508535a8a191822c7e208223b78cd259209
luatoolz <luatoolz@somahorse.com>
2024-08-19 00:50:49 +0300

    return "always nil" iterator for incorrent arguments

--- lua/t/storage/mongo/iter.lua
+++ lua/t/storage/mongo/iter.lua
@@ -1,8 +1,8 @@
-local t = require "t"
 local getmetatable = debug.getmetatable or getmetatable
 return function(cursor, handler)
   if type(cursor)=='table' or type(cursor)=='userdata' then
     local _iter = (getmetatable(cursor or {}) or {}).__iter
     if type(_iter)=='function' then return _iter(cursor, handler) end
   end
+  return function() return nil end
 end
