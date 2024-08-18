ebaffd0bf85543375757011de7323c7d22eb8eca
luatoolz <luatoolz@somahorse.com>
2024-08-19 00:59:21 +0300

    fix: t/init.d/mongo_cursor

--- lua/t/init.d/mongo_cursor.lua
+++ lua/t/init.d/mongo_cursor.lua
@@ -1,2 +1,5 @@
-require 't'
+-- using global t to prevent require "t" loop
+-- global t should always be defined because of init.d
+local t = assert(t)
+-- load using full name to prevent t/storage/mongo/init.lua loop
 require 't.storage.mongo.cursor'
