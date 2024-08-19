1082815e57ae41ffdda6136e5e9cfb9879fd1608
luatoolz <luatoolz@somahorse.com>
2024-08-19 09:21:48 +0300

    better query

--- lua/t/storage/mongo/records.lua
+++ lua/t/storage/mongo/records.lua
@@ -8,8 +8,7 @@ local tocursor = require "t.storage.mongo.cursor"
 return setmetatable({}, {
   __call=function(self, coll, query)
     assert(is.storage.mongo.collection(coll))
-    assert(query)
-    if query=='*' then query={} end
+    if query=='*' or query=='' or type(query)=='nil' then query={} end
     return setmetatable({__=coll.__, coll=coll, query=query}, getmetatable(self))
   end,
   __eq=function(self, to) return t.type(self)==t.type(to) and self.coll and tostring(self.coll)==tostring(to.coll) and is.eq(self.query, to.query) end,
@@ -21,10 +20,10 @@ return setmetatable({}, {
   __name='t/storage/mongo/records',
   __pairs=function(self) local i, it = 0, iter(self, jsoner)
     return function() local rv=it(); i=rv and i+1 or nil; return i,rv end end,
-  __sub=function(self, o)
-    if rawequal(self, o) or o=='*' or is.table.empty(o) then o=self.query end
-    if type(o)=='number' then return self - self[o] end
-    if o then return self.__:remove(o) end
+  __sub=function(self, query)
+    if query=='*' or query=='' or type(query)=='nil' then query={} end
+    if rawequal(self, query) then query={} end
+    if query then return self.__:remove(query) end
   end,
   __toboolean=function(self) return tonumber(self)>0 end,
   __tonumber=function(self) return self.__:count(self.query) or 0 end,
