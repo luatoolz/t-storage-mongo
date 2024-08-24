0d7027b60159c1c190ec50d3aee728bc701f83e2
luatoolz <luatoolz@somahorse.com>
2024-08-24 08:16:38 +0300

    fixup driver.ObjectID in oid.lua

--- lua/t/storage/mongo/oid.lua
+++ lua/t/storage/mongo/oid.lua
@@ -5,16 +5,7 @@ local is = t.is
 local json = t.format.json
 local getmetatable = debug.getmetatable or getmetatable
 
-local mt = getmetatable(oid())
-if type(mt.__tojson)=='nil' then
-  mt.__tojson = function(self)
-    return tostring(self)
-  end
-  mt.__toJSON = mt.__tojson
-end
-
--- TODO normalize
-return function(x)
+local ObjectID = function(x)
   if type(x)=='nil' then return oid() end
   if type(x)=='userdata' and t.type(x)=='mongo.ObjectID' then return x end
   if type(x)=='string' then
@@ -27,7 +18,18 @@ return function(x)
       if type(x._id)=='table' then x=x._id end
       if is.oid(x['$oid']) then return oid(x['$oid']) end
     else
-      if is.stringer(x) then local tx=tostring(x); if is.oid(tx) then return end end
+      if is.stringer(x) then local tx=tostring(x); if is.oid(tx) then return oid(tx) end end
     end
   end
 end
+
+local mt = getmetatable(oid())
+if mt and type(mt.__tojson)=='nil' then
+  mt.__tojson = function(self)
+    return tostring(self)
+  end
+  mt.__toJSON = mt.__tojson
+  driver.ObjectID = ObjectID
+end
+
+return ObjectID
