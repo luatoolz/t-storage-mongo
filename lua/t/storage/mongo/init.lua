77fbdbd235e79d5de61cd60a3711c16d2544da53
luatoolz <luatoolz@somahorse.com>
2024-12-10 07:32:14 +0300

    t.object -> mt.factory

--- lua/t/storage/mongo/init.lua
+++ lua/t/storage/mongo/init.lua
@@ -1,14 +1,17 @@
 local t=t or require "t"
 require "t.format.bson"
-local is = t.is ^ 'mongo'
+local _ = t.is ^ 'mongo'
 local to = t.to
+local mt = t.mt
 local iter = table.iter
 local pkg = t.pkg(...)
 local storage = pkg.cache
 local connection = pkg.connection
 local client = pkg.client
 
-return t.object({
+local mongo={}
+local factory = function(x) return rawequal(mongo, x) and true or nil end
+return setmetatable(mongo,{
   __name=tostring(pkg),
   __call=function(self, connstr) return setmetatable({connstr=connstr}, getmetatable(self)) end,
   __pow=function(self, x) -- tie storage to container [loader or any indexable]
@@ -16,16 +19,19 @@ return t.object({
     storage[x]=self
     return self
   end,
-  __toboolean=function(self) return to.boolean((is.factory(self) and self() or self)._client) end,
+  __toboolean=function(self) return to.boolean((factory(self) and self() or self)._client) end,
   __div=function(self, dbname) return assert(self._client/dbname or self._dbname) end,
   __iter=function(self) return iter(self._client) end,
+  __index=mt.factory,
   __pairs=function(self) return pairs(self._db)  end,
-}):computable({
-  _conn=function(self) return connection(rawget(self, 'connstr')) end,
-  _client=function(self) return client(self._conn) end,
-  _dbname=function(self) return self._conn.db end,
-  _db=function(self) return assert(self._client/self._dbname) end,
-}):postindex(function(self, k)
-  if is.factory(self) then return self()[k] end
-  return (self._db or {})[k]
-end):loader(tostring(pkg)):factory()
+  __computable={
+    _conn=function(self) return connection(rawget(self, 'connstr')) end,
+    _client=function(self) return client(self._conn) end,
+    _dbname=function(self) return self._conn.db end,
+    _db=function(self) return assert(self._client/self._dbname) end,
+  },
+  __postindex=function(self, k)
+    if factory(self) then return self()[k] end
+    return (self._db or {})[k]
+  end,
+})
