2b9f07881ca16612db811c5815895ecd28636621
luatoolz <luatoolz@somahorse.com>
2025-01-25 09:58:52 +0300

    better connection

--- lua/t/storage/mongo/connection.lua
+++ lua/t/storage/mongo/connection.lua
@@ -4,26 +4,18 @@ local env = t.env
 local pkg = t.pkg(...)
 
 local escape = pkg.escape
+local function join(sep, ...) if type(sep)=='string' then return sep:join(...) end end
 
-env({
-  MONGO_PREFIX='mongodb://',
-  MONGO_HOST='mongodb',
-  MONGO_PORT='27017',
-  MONGO_DB='db',
---MONGO_USER
---MONGO_PASS
---MONGO_DB
---MONGO_OPTIONS
---MONGO_CONNSTRING
-})
-
-local function join(sep, ...)
-  sep=sep or ''
-  local args={}
-  for i=1,select('#', ...) do if select(i, ...)~=nil then args[#args+1]=select(i, ...) end end
-  local rv = table.concat(args, sep)
-  return rv~='' and rv or nil
-end
+env.mongo = {
+  prefix  = 'mongodb://',
+  host    = 'mongodb',
+  port    = '27017',
+  db      = 'db',
+  user    = true,
+  pass    = true,
+  options = true,
+  connstring = true,
+}
 
 -- mongodb://user:pass@othermongo:27017/db?x=y&a=b
 local function parse_connstring(oconnstring)
@@ -65,25 +57,26 @@ local function parse_connstring(oconnstring)
 end
 
 return t.factory({},{
-  __name='connection',
-  __tostring=function(self) return (is.factory(self) and self() or self).connstring or '' end,
-  __call=function(self, conn)
-    if type(conn)=='string' then conn=parse_connstring(conn) end
-    if type(conn)~='table' then conn={} end
-    return getmetatable(conn)==getmetatable(self) and conn or setmetatable(conn, getmetatable(self))
-  end,
-  __computable={
+__name='connection',
+__tostring=function(self) return (is.factory(self) and self() or self).connstring or '' end,
+__call=function(self, conn)
+  if type(conn)=='string' then conn=parse_connstring(conn) end
+  if type(conn)~='table' then conn=env.mongo or {} end
+  return getmetatable(conn)==getmetatable(self) and conn or setmetatable(conn, getmetatable(self))
+end,
+__computable={
   prefix  = function(self) return env.MONGO_PREFIX end,
   db      = function(self) if next(self)==nil then return env.MONGO_DB end end,
   hosts   = function(self) return env.MONGO_HOST:match('[,]') and env.MONGO_HOST:split(',') or nil end,
   host    = function(self) return (not self.hosts) and (env.MONGO_HOST or ''):nmatch('^[^:]*'):null() or nil end,
   port    = function(self) if next(self)==nil then return env.MONGO_PORT or (env.MONGO_HOST or ''):nmatch('%d*$'):null() end end,
   user    = function(self) if next(self)==nil then return env.MONGO_USER end end,
-  pass    = function(self) if next(self)==nil then return escape(env.MONGO_PASS) end end,
+  pass    = function(self) if next(self)==nil then return env.MONGO_PASS end end,
+  pass_escaped = function(self) return escape(self.pass) end,
   options = function(self) return (env.MONGO_OPTIONS or ''):lstrip('?'):null() end,
-  connstring=function(self)
-    local cred = join(':', self.user, self.pass)
-    local host = self.hosts and table.concat(self.hosts, ','):null() or join(':', self.host, self.port)
+  connstring = function(self)
+    local cred = join(':', self.user, self.pass_escaped)
+    local host = self.hosts and join(',', self.hosts) or join(':', self.host, self.port)
     local credhost = join('@', cred, host)
     local db = self.db
     local hostdb = join('/', credhost, db)
@@ -92,5 +85,4 @@ return t.factory({},{
     local hostopts = join('?', hostdb, options)
     return join('', self.prefix, hostopts)
   end,
-  },
-})
+},})
